7d2f3442456db5909b8c5c89697fba93504fdf09
Adam Krol <adam.krol@idemia.com>
2020-04-12 14:43:25 +0200

    Fixed url for package

--- setup.py
+++ setup.py
@@ -5,19 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='wolf_smartset',
-    version='0.1.2',
+    version='0.1.3',
     author="Adam Krol",
     author_email="adam.krol93@gmail.com",
     description="A package to comunicate with Wolf Smart Set Cloud",
     long_description=long_description,
     package_data={"wolf_smartset": ["py.typed"]},
     long_description_content_type="text/markdown",
-    # url="https://github.com/javatechy/dokr",
+    url="https://github.com/adamkrol93/wolf-smartset",
     include_package_data=True,
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        # "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     install_requires=[
