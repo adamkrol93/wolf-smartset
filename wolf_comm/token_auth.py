09494b20277be9188cbef0f30c532cce8759ce30
Jan Rothkegel <jan.rothkegel@web.de>
2024-02-11 09:08:59 +0100

    Add exception handling for authentication

--- wolf_comm/token_auth.py
+++ wolf_comm/token_auth.py
@@ -31,79 +31,80 @@ class TokenAuth:
         self.password = password
 
     async def token(self, session: AsyncClient) -> Tokens:
+        try:
+            # Generate client-sided variables for OpenID
+            code_verifier, code_challenge = pkce.generate_pkce_pair()
+            state = shortuuid.uuid()
         
-        # Generate client-sided variables for OpenID
-        code_verifier, code_challenge = pkce.generate_pkce_pair()
-        state = shortuuid.uuid()
-      
 
-        # Retrieve verification token from WOLF website
-        r = await session.get(constants.AUTHENTICATION_BASE_URL + '/Account/Login?ReturnUrl=/idsrv/connect/authorize/callback?client_id={}&redirect_uri={}/signin-callback.html&response_type=code&scope=openid%2520profile api role&state={}&code_challenge={}&code_challenge_method=S256&response_mode=query&lang=de-DE'.format(constants.AUTHENTICATION_CLIENT, constants.BASE_URL,state, code_challenge))
+            # Retrieve verification token from WOLF website
+            r = await session.get(constants.AUTHENTICATION_BASE_URL + '/Account/Login?ReturnUrl=/idsrv/connect/authorize/callback?client_id={}&redirect_uri={}/signin-callback.html&response_type=code&scope=openid%2520profile api role&state={}&code_challenge={}&code_challenge_method=S256&response_mode=query&lang=de-DE'.format(constants.AUTHENTICATION_CLIENT, constants.BASE_URL,state, code_challenge))
 
-        _LOGGER.debug('Verification code response: %s', r.content)
+            _LOGGER.debug('Verification code response: %s', r.content)
 
-        tree = html.document_fromstring(r.text)
-        elements = tree.xpath('//form/input/@value')
+            tree = html.document_fromstring(r.text)
+            elements = tree.xpath('//form/input/@value')
 
-        verification_token = elements[0] # __RequestVerificationToken
+            verification_token = elements[0] # __RequestVerificationToken
 
-        # Get code
-        login_data = {
-            "Input.Username": self.username,
-            "Input.Password": self.password,
-            "__RequestVerificationToken": verification_token
-        }
+            # Get code
+            login_data = {
+                "Input.Username": self.username,
+                "Input.Password": self.password,
+                "__RequestVerificationToken": verification_token
+            }
 
-        r = await session.post(
-            constants.AUTHENTICATION_BASE_URL + "/Account/Login",
-            params={
-                "ReturnUrl": constants.AUTHENTICATION_URL + "/connect/authorize/callback?client_id={}&redirect_uri={}/signin-callback.html&response_type=code&scope=openid profile api role&state={}&code_challenge={}&code_challenge_method=S256&response_mode=query&lang=de-DE".format(constants.AUTHENTICATION_CLIENT, constants.BASE_URL, state,code_challenge)
-            },
-            headers={
+            r = await session.post(
+                constants.AUTHENTICATION_BASE_URL + "/Account/Login",
+                params={
+                    "ReturnUrl": constants.AUTHENTICATION_URL + "/connect/authorize/callback?client_id={}&redirect_uri={}/signin-callback.html&response_type=code&scope=openid profile api role&state={}&code_challenge={}&code_challenge_method=S256&response_mode=query&lang=de-DE".format(constants.AUTHENTICATION_CLIENT, constants.BASE_URL, state,code_challenge)
+                },
+                headers={
+                    "Sec-Fetch-Dest": "document",
+                    "Sec-Fetch-Mode": "navigate",
+                },
+                data=login_data,
+                cookies = r.cookies,
+                follow_redirects=True
+            )
+            
+            _LOGGER.debug('Code response: %s', r.content)
+            code = r.url.params['code']
+            
+
+            headers = {
+                "Cache-control": "no-cache",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:108.0) Gecko/20100101 Firefox/108.0",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                "Accept-Language": "de-DE,de;q=0.8,en-US;q=0.5,en;q=0.3",
+                "Referer": constants.BASE_URL + "/",
                 "Sec-Fetch-Dest": "document",
                 "Sec-Fetch-Mode": "navigate",
-            },
-            data=login_data,
-            cookies = r.cookies,
-            follow_redirects=True
-        )
-        
-        _LOGGER.debug('Code response: %s', r.content)
-        code = r.url.params['code']
-        
+                "Sec-Fetch-Site": "same-origin",
+                "TE": "trailers"
+            }
 
-        headers = {
-            "Cache-control": "no-cache",
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:108.0) Gecko/20100101 Firefox/108.0",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-            "Accept-Language": "de-DE,de;q=0.8,en-US;q=0.5,en;q=0.3",
-            "Referer": constants.BASE_URL + "/",
-            "Sec-Fetch-Dest": "document",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-Site": "same-origin",
-            "TE": "trailers"
-        }
-
-        # Get token
-        r = await session.post(
-            constants.AUTHENTICATION_BASE_URL + "/connect/token",
-            headers=headers,
-            data={
-                "client_id": "smartset.web",
-                "code": code,
-                "redirect_uri": constants.BASE_URL + "/signin-callback.html",
-                "code_verifier": code_verifier,
-                "grant_type": "authorization_code",
-            },
-        )
-                
-        json = r.json()
-        _LOGGER.debug('Token response: %s', json)
-        if "error" in json:
+            # Get token
+            r = await session.post(
+                constants.AUTHENTICATION_BASE_URL + "/connect/token",
+                headers=headers,
+                data={
+                    "client_id": "smartset.web",
+                    "code": code,
+                    "redirect_uri": constants.BASE_URL + "/signin-callback.html",
+                    "code_verifier": code_verifier,
+                    "grant_type": "authorization_code",
+                },
+            )
+                    
+            json = r.json()
+            _LOGGER.debug('Token response: %s', json)
+            if "error" in json:
+                raise InvalidAuth
+            return Tokens(json.get("access_token"), json.get("refresh_token"), json.get("expires_in"))
+        except:
             raise InvalidAuth
-        return Tokens(json.get("access_token"), json.get("refresh_token"), json.get("expires_in"))
-
 
 class InvalidAuth(Exception):
-    """Invalid username and password was passed"""
+    """Please check whether you entered an invalid username or password. If everything looks fine then probably there is an issue with Wolf SmartSet servers."""
     pass
