1152cb0bd14ca7f78da7c8aacfa381d07260af24
Jan Rothkegel <jan.rothkegel@web.de>
2024-02-11 09:21:47 +0100

    Increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='wolf_comm',
-    version='0.0.1',
+    version='0.0.2',
     author="Jan Rothkegel",
     author_email="jan.rothkegel@web.de",
     description="A package to communicate with Wolf SmartSet Cloud",
